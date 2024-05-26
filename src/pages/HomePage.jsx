37e17a583c637b3a66aa09020fb8e7e58170c217
Nirmal Kumar Gupta <92977085+The-ZeGOD@users.noreply.github.com>
2024-05-26 15:58:33 +0530

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Hero from '../components/Hero'
 import HomeCards from '../components/HomeCards'
 import JobListings from '../components/JobListings'
-import VierAllJobs from '../components/ViewAllJobs'
 import ViewAllJobs from '../components/ViewAllJobs'
 
 const HomePage = () => {
